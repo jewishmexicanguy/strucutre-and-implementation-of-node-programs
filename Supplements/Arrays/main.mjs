22bf9b28573de247b504cdb3bda8c111a11f94bb
matthewricklefs <ricklefsmatthew1003@gmail.com>
2021-06-02 12:12:04 -0400

    makes progress within Objective 1, shows variants of the thought process taken, pushes before cleaning up the code and moving on with the Assignment

--- Supplements/Arrays/main.mjs
+++ Supplements/Arrays/main.mjs
@@ -1,53 +1,144 @@
-import util from '../util.mjs';
+import util from "../util.mjs";
 
 function generateNumbers(length, min, max) {
   var cardinality = [];
   var numbers = [];
+  var result = {};
+
   for (length; length > 0; length--) {
     cardinality.push(length);
     numbers.push(util.getRandomInt(min, max));
   }
-  return {
-    cardinality, 
-    numbers
-  };
-}
 
-function generateMatrixNumbers(mattrixLength, length, min, max) {
-  var subspaces = [];
-  for (mattrixLength; mattrixLength > 0; mattrixLength--) {
-    var sequence = [];
-    for (var i = length; i > 0; i--) {
-      sequence.push(util.getRandomInt(min, max));
-    }
-    subspaces.push(sequence);
+  // Sorting the Cardinality Array in Ascending Order
+  for (var i = 0; i < cardinality.length; i++) {
+    cardinality.sort(function (a, b) {
+      return a - b;
+    });
   }
-  return subspaces;
-}
 
-function generateJaggedMattrix(mattrixLength, lengthMax, min, max) {
-  var subspaces = [];
-  for (mattrixLength; mattrixLength > 0; mattrixLength--) {
-    var sequence = [];
-    for (var i = util.getRandomInt(1, lengthMax); i > 0; i--) {
-      sequence.push(util.getRandomInt(min, max));
-    }
-    subspaces.push(sequence);
+  // Sorting the Numbers Array in Ascending Order
+  for (var i = 0; i < numbers.length; i++) {
+    numbers.sort(function (a, b) {
+      return a - b;
+    });
   }
-  return subspaces;
+
+  // Combining the Two Arrays by their Index Values into their own Objects inside of an Array
+  var result = numbers.map(function (obj, index) {
+    var myObj = {};
+    myObj[cardinality[index]] = obj;
+    return myObj;
+  });
+
+  /**
+   *   Attemping with forEach method
+   */
+  // cardinality.forEach((key, i) => {
+  //   result[key] = numbers[i];
+  // });
+
+
+  /**
+   * Object.fromEntries ES2018 method
+   */
+  // result = Object.fromEntries(cardinality.map((_, i) => ["cardinal: " + cardinality[i], "number: " + numbers[i]]))
+
+
+  /**
+   * Attemping with map method
+   */
+  // cardinality.map((key, i) => {
+  //   result[key] = numbers[i];
+  // });
+
+
+  /**
+   * Attempting with reduce method
+   */
+  // let combineArrays = (first, second) => {
+  //   return first.reduce((acc, val, index) => {
+  //     {
+  //       acc[val] = second[index];
+  //     }
+  //     return acc;
+  //   }, {});
+  // };
+
+  return result;
+
+  // return {
+  //   cardinality,
+  //   numbers,
+  // };
 }
 
 const length = 1000;
-var cardinality, numbers = generateNumbers(length, 1, 1000000);
 
-console.log(cardinality);
-console.log(numbers);
+// var cardinality,
+//   numbers = generateNumbers(length, 1, 1000000);
+var result = generateNumbers(length, 1, 1000000);
 
-var evenMattrix = generateMatrixNumbers(10, 10, 1, 1000);
-var jaggedMattrix = generateJaggedMattrix(10, 20, 1, 1000000);
+console.log(result);
+// console.log(cardinality);
+// console.log(numbers);
 
-console.log(evenMattrix);
-console.log(jaggedMattrix);
+/**
+ * Attempting to seperate the Object with
+ * .keys() and .values() methods
+ */
+
+// const seperateObject = (result) => {
+//   const res = [];
+//   const keys = Object.keys(result);
+//   const values = Object.values(result);
+
+//   keys.forEach((key) => {
+//     res.push({
+//       cardinal: result[key],
+//     });
+//   });
+
+//   // values.forEach((value) => {
+//   //   res.push({
+//   //     number: result[value],
+//   //   });
+//   // });
+
+//   return res;
+// };
+
+// console.log(seperateObject(result));
+
+// function generateMatrixNumbers(mattrixLength, length, min, max) {
+//   var subspaces = [];
+//   for (mattrixLength; mattrixLength > 0; mattrixLength--) {
+//     var sequence = [];
+//     for (var i = length; i > 0; i--) {
+//       sequence.push(util.getRandomInt(min, max));
+//     }
+//     subspaces.push(sequence);
+//   }
+//   return subspaces;
+// }
+
+// function generateJaggedMattrix(mattrixLength, lengthMax, min, max) {
+//   var subspaces = [];
+//   for (mattrixLength; mattrixLength > 0; mattrixLength--) {
+//     var sequence = [];
+//     for (var i = util.getRandomInt(1, lengthMax); i > 0; i--) {
+//       sequence.push(util.getRandomInt(min, max));
+//     }
+//     subspaces.push(sequence);
+//   }
+//   return subspaces;
+// }
+
+// var evenMattrix = generateMatrixNumbers(10, 10, 1, 1000);
+// var jaggedMattrix = generateJaggedMattrix(10, 20, 1, 1000000);
+
+// console.log(evenMattrix);
+// console.log(jaggedMattrix);
 
 /**
  * Goal
@@ -60,11 +151,11 @@ console.log(jaggedMattrix);
  * Objetive 1
  * The variables "cardinality" and "numbers" are arrays. However they are ordered in the wrong direction, counting down as opposed to counting up.
  * You may assume that the "cardinality" is a continous count.
- *  
+ *
  * Using a for loop, iterate through variables "cardinality" and "numbers".
  * Producing an array that contains objects that includes the cardinality/index of each item in the number array and the number itself.
  * EX [
- *      {"cardinal":1, "number":51}, 
+ *      {"cardinal":1, "number":51},
  *      ...
  *    ]
  * Caveat order the objects in array counting up as opposed to the current order of "cardinality" and "numbers".
